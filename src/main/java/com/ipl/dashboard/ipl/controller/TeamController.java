da697ab9c876523b9839d0c55b28dfd36897a8bf
Revanth Kumar <RevanthKumarM@users.noreply.github.com>
2021-05-29 14:42:54 +0530

    Created api endpoint for IPL data

---
+++ src/main/java/com/ipl/dashboard/ipl/controller/TeamController.java
@@ -0,0 +1,30 @@
+package com.ipl.dashboard.ipl.controller;
+
+import com.ipl.dashboard.ipl.model.Team;
+import com.ipl.dashboard.ipl.repository.MatchRepository;
+import com.ipl.dashboard.ipl.repository.TeamRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        Team team = this.teamRepository.findByTeamName(teamName);
+        
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName,4));
+        return team;
+    }
+    
+}

8480dfdce39ce9af914c06f16c526792809fbdaa
Revanth Kumar <RevanthKumarM@users.noreply.github.com>
2021-06-05 22:18:48 +0530

    completed UI elements for the teampage

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 
 export const TeamPage = () => {
 
-  const [team, setTeam] = useState({matches:[]});
+  const [team, setTeam] = useState({ matches: [] });
   const { teamName } = useParams();
 
   useEffect(
@@ -17,15 +17,19 @@ export const TeamPage = () => {
         setTeam(data);
       };
       fetchMatches();
-    }, []
+    }, [teamName]
   );
+
+  if (!team || !team.teamName) {
+    return <h1>Team Not Found</h1>
+  }
 
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      
-      <MatchDetailCard match={team.matches[0]}/>
-      {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)}
     </div>
   );
 }
